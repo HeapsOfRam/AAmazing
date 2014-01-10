e4a9bc89348cd245ee7412d66519688f74e516f4
Ryan March <rmarch03@villanova.edu>
2014-01-10 17:35:32 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Animezing::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root 'pages#home'
 
-  match '/help',   to: 'pages#help',  via: 'get'
-  match '/about',  to: 'pages#about', via: 'get'
+  match '/help',    to: 'pages#help',       via: 'get'
+  match '/about',   to: 'pages#about',      via: 'get'
 
-  match '/signup', to: 'users#new',   via: 'get'
+  match '/signup',  to: 'users#new',        via: 'get'
+  match '/signin',  to: 'sessions#new',     via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
